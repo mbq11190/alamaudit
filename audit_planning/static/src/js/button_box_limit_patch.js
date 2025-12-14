bf4c0ffbdb028703830b9993cdcc5da9abd4d4af
aqeelalamfca-sys <aqeelalamfca@gmail.com>
2025-12-15 03:54:03 +0400

    Update button_box_limit_patch.js

--- audit_planning/static/src/js/button_box_limit_patch.js
+++ audit_planning/static/src/js/button_box_limit_patch.js
@@ -2,11 +2,11 @@
 import { patch } from "@web/core/utils/patch";
 import { FormButtonBox } from "@web/views/form/button_box/button_box";
 
-// Increase smart button inline limit so all phase buttons stay visible without "More" dropdown.
+// Raise smart-button inline limit so buttons render without the "More" dropdown.
 const superSetup = FormButtonBox.prototype.setup;
 patch(FormButtonBox.prototype, "audit_planning.button_box_limit", {
     setup() {
-        this.buttonLimit = 8;
         superSetup.call(this);
+        this.buttonLimit = 20;
     },
 });
