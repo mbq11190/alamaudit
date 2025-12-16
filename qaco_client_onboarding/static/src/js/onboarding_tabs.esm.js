7b07063c80271b1432bb2b4105441fda14a180e5
aqeelalamfca-sys <aqeelalamfca@gmail.com>
2025-12-16 14:59:11 +0500

    Update onboarding_tabs.esm.js

--- qaco_client_onboarding/static/src/js/onboarding_tabs.esm.js
+++ qaco_client_onboarding/static/src/js/onboarding_tabs.esm.js
@@ -10,7 +10,7 @@ const superSetup = FormRenderer.prototype.setup;
 function isDebugAssets() {
     try {
         return /(^|[?&])debug=assets([&#]|$)/.test(window.location.search || "");
-    } catch {
+    } catch (e) {
         return false;
     }
 }
