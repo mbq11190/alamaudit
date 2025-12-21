a7f8edb4207628e31663be18d56449203aa1568f
aqeelalamfca-sys <aqeelalamfca@gmail.com>
2025-12-21 21:49:31 +0500

    fix: normalize odoo.define modules and make client JS ES5-compatible to avoid asset parse errors

--- ai_audit_management/static/src/js/chatbot_widget.js
+++ ai_audit_management/static/src/js/chatbot_widget.js
@@ -1,11 +1,15 @@
 /** Minimal placeholder for chatbot trigger; extend with OWL component if desired. */
-odoo.define('ai_audit_management.chatbot_widget', function (require) {
+odoo.define('ai_audit_management.chatbot_widget', ['@odoo/owl'], function (owl) {
     "use strict";
-    const { Component } = require('@odoo/owl');
+    var Component = owl.Component;
 
-    class ChatbotWidget extends Component {
-        static template = 'ai_audit_management.ChatbotWidget';
+    // ES5-compatible component definition
+    function ChatbotWidget() {
+        Component.apply(this, arguments);
     }
+    ChatbotWidget.prototype = Object.create(Component.prototype);
+    ChatbotWidget.prototype.constructor = ChatbotWidget;
+    ChatbotWidget.template = 'ai_audit_management.ChatbotWidget';
 
     return ChatbotWidget;
 });
