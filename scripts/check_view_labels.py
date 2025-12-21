ad41de992f948d5ad1edac011dbf8e55438cea97
aqeelalamfca-sys <aqeelalamfca@gmail.com>
2025-12-21 15:06:30 +0500

    10

---
+++ scripts/check_view_labels.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+import glob
+
+problems = []
+files = glob.glob('**/views/*.xml', recursive=True)
+for f in files:
+    try:
+        tree = ET.parse(f)
+    except Exception:
+        continue
+    root = tree.getroot()
+    for record in root.findall('.//record'):
+        if record.get('model') != 'ir.ui.view':
+            continue
+        arch = record.find('field[@name="arch"]')
+        if arch is None:
+            continue
+        for label in arch.findall('.//label'):
+            has_for = 'for' in label.attrib
+            has_class = 'class' in label.attrib and 'o_form_label' in label.attrib.get('class','')
+            if not has_for and not has_class:
+                problems.append((f, record.get('id'), ET.tostring(label, encoding='unicode')))
+
+print('Problems found:', len(problems))
+for p in problems:
+    print(p[0], p[1], p[2])
