f7b8bcf610b6020f45843232e6ed80c1b6cde300
aqeelalamfca-sys <aqeelalamfca@gmail.com>
2025-12-23 17:49:16 +0500

    Create odoo.__DEBUG__.services['web.core'].js

---
+++ web_responsive/static/src/legacy/js/odoo.__DEBUG__.services['web.core'].js
@@ -0,0 +1,7 @@
+odoo.__DEBUG__.services['web.core']._services.module_loader.modulesJSON.stringify(
+  window.__missing_module_definitions ||
+  window.__missing_module_shims ||
+  [],
+  null,
+  2
+)
